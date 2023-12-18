4971b98499b9f8c2578c5dee42f07602425558f3
Ramayanam sai kiran <145695560+saikiranramayanam@users.noreply.github.com>
2023-12-18 11:04:01 +0530

    #include<stdio.h> int main() { 	int a,i; 	scanf("%d",&a); 	int arr[a]; 	for (i=0;i<a;i++) 	{ 		scanf("%d",&arr[i]); 	} 	int max=arr[0]; 	for (i=1;i<a;i++) 	{ 		if (max<arr[i]) 		max=arr[i]; 	} 	printf("%d",max); }

---
+++ Maximum_of_an_Array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,i;
+	scanf("%d",&a);
+	int arr[a];
+	for (i=0;i<a;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int max=arr[0];
+	for (i=1;i<a;i++)
+	{
+		if (max<arr[i])
+		max=arr[i];
+	}
+	printf("%d",max);
+}
