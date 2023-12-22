bedc46a128ee457233fe18a3118350a916bf0b5f
Ramayanam sai kiran <145695560+saikiranramayanam@users.noreply.github.com>
2023-12-22 20:43:51 +0530

    #include<stdio.h> int main() { 	int r,a,sum=0; 	scanf("%d",&a); 	while (a>0) 	{ 		r=a%10; 		break; 	} 	while (a>0) 	{ 		if (a<10) 		{ 		sum=a%10; 		break; 	    } 	    else 	    a=a/10; 	} 	printf("%d",sum+r); }

--- Sum_of__First_and_Last_digits_.c
+++ Sum_of__First_and_Last_digits_.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
-int main(){
-    int n,count=0;
-    scanf("%d",&n);
-    int b=n%10;
-    for(int a=n;a>0;a=a/10){
-        int c=a%10;
-        if(a<10){
-            printf("%d",a+b);
-        }
-    }
+int main()
+{
+	int r,a,sum=0;
+	scanf("%d",&a);
+	while (a>0)
+	{
+		r=a%10;
+		break;
+	}
+	while (a>0)
+	{
+		if (a<10)
+		{
+		sum=a%10;
+		break;
+	    }
+	    else 
+	    a=a/10;
+	}
+	printf("%d",sum+r);
 }
